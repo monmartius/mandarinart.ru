d218b2cc4d998585a3ba2cdae8b84ac33bdfbae2
monmartius <monmarius@gmail.com>
2019-02-25 16:27:04 +0700

    + copy-webpack-plugin

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./bootstrap/bootstrap.js";
+import './styles.scss';
+// import "./about.html";
+
+let a = 1;
+
+jQuery(document).ready(function($){
+	$('#test').html('<h1>It works!!</h1>').css('background-color', 'red');
+	alert('!!!!!!!!!!!!!!!');
+});
+
+
+
