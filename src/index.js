06b87746e4edcf2791517f97ae647e82bc322b48
monmartius <monmartius@gmail.com>
2019-02-26 23:08:25 +0700

    slick-slider!

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@ import './styles.scss';
 // import "./about.html";
 
 
+require('slick-carousel');
+require('slick-carousel/slick/slick.scss');
+require('slick-carousel/slick/slick-theme.scss');
 
+const $ = require('jquery');
+$(document).ready(function(){
+
+    let $slick1 = $('#slick1  .items').slick({
+        // dots: true,
+        // infinite: true,
+        // speed: 1500,
+        slidesToShow: 1,
+        // autoplay: true,
+        // autoplaySpeed: 3000
+
+    });
+});
 
--- webpack.config.js
+++ webpack.config.js
@@ -59,22 +59,34 @@ module.exports = {
 						}
 					}
 				]
-			}
+			},
 
-			// {
-			// 	test: /\.css$/,
-			// 	use: [
-			// 		{
-			// 			loader: MiniCssExtractPlugin.loader
-			// 			// options: {
-			// 			//	 // you can specify a publicPath here
-			// 			//	 // by default it use publicPath in webpackOptions.output
-			// 			//	 publicPath: '../'
-			// 			// }
-			// 		},
-			// 		"css-loader"
-			// 	]
-			// }
+
+
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: 'file-loader',
+				options: {
+					name: './fonts/[name].[ext]',
+				},
+			},
+
+
+
+			{
+				test: /\.css$/,
+				use: [
+					// {
+					// 	loader: MiniCssExtractPlugin.loader
+					// 	// options: {
+					// 	//	 // you can specify a publicPath here
+					// 	//	 // by default it use publicPath in webpackOptions.output
+					// 	//	 publicPath: '../'
+					// 	// }
+					// },
+					"css-loader"
+				]
+			}
 		]
 	},
 
